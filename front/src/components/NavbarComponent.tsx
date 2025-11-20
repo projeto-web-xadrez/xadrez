9bdab419de16c44328e42f308e5ad8dc8aeed042
caiogcz <caiocoutinho04@gmail.com>
2025-11-20 16:59:11 -0300

    removed signoutcomponent, added css for nav bar and readded sign out button inside the nav bar

--- front/src/components/NavbarComponent.tsx
+++ front/src/components/NavbarComponent.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import '../styles/nav-bar-styles.css'
 
 function Navbar() {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, username, logout } = useAuth();
+    const [open, setOpen] = useState(false);
 
     return (
-        <nav className='nav'>
-            <Link to="/">Home</Link>
-            <Link to="/leaderboard">Leaderboard</Link>
-            {isAuthenticated ? (
-                <>
-                    <Link to="/dashboard">Dashboard</Link>
+        <nav className='navbar'>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/leaderboard">Leaderboard</Link>
+                {isAuthenticated ? (
+                    <>
+                        <Link to="/dashboard">Dashboard</Link>
 
-                </>
-            ) : (
-                <>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
 
+                )}
+            </div>
+            {isAuthenticated && (
+                <div className="profile">
+                    <button
+                        className="profile-btn"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {username}
+                    </button>
+
+                    <div className={`profile-menu ${open ? "open" : ""}`}>
+                        <button onClick={logout}>Sign Out</button>
+                    </div>
+                </div>
             )}
         </nav>
     )
