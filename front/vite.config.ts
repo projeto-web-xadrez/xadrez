a042275c9f21c600a93c4d47db48e1f46f85df9e
Diogo Hyppolito de Andrade <diogohandrad@gmail.com>
2025-12-10 15:22:42 -0300

    Setting up branch for deploying

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      'www.xadrezweb.online',
+      'xadrezweb.online',
+      'localhost'
+    ]
+  },
   plugins: [
     react({
       babel: {
